9d4505e98e11a6863eae55b3237b29dc2223b7ca
Ehsan Miraly <ehsanmiraly@gmail.com>
2026-07-02 12:33:04 -0700

    Fix Labeled Slider

--- Assets/App/Scripts/UI/Screen_UI/SettingsPage.cs
+++ Assets/App/Scripts/UI/Screen_UI/SettingsPage.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,7 +11,15 @@ public class SettingsPage : MonoBehaviour
     ScrollView settings_ScrollView;
 
     VisualElement languagePreviousNextSelector_TemplateContainer;
+    VisualElement language_ChevronLeft_TemplateContainer;
+    Label language_Lable;
+    VisualElement language_ChevronRight_TemplateContainer;
+
     VisualElement fontSizePreviousNextSelector_TemplateContainer;
+    VisualElement fontSize_ChevronLeft_TemplateContainer;
+    Label fontSize_Lable;
+    VisualElement fontSize_ChevronRight_TemplateContainer;
+
 
 
     private void OnEnable()
@@ -26,7 +35,7 @@ public class SettingsPage : MonoBehaviour
 
     private void OnDisable()
     {
-        //RemoveFunctionality();
+        RemoveFunctionality();
         panelRenderer.UnregisterUIReloadCallback(OnUIReloadCallback);
 
         EventsManager.OnLanguageChanged_Event -= OnLanguageChanged;
@@ -39,13 +48,27 @@ public class SettingsPage : MonoBehaviour
         settings_ScrollView = settingsPage_VisualElement.Q<ScrollView>("Settings_ScrollView");
         UI_Utilities.Initialize_ScrollView(settings_ScrollView);
 
-        languagePreviousNextSelector_TemplateContainer =
-            settings_ScrollView.Q<VisualElement>("LanguagePreviousNextSelector_TemplateContainer");
+        //Language
+        languagePreviousNextSelector_TemplateContainer = settings_ScrollView.
+            Q<VisualElement>("LanguagePreviousNextSelector_TemplateContainer");
+        language_ChevronLeft_TemplateContainer = languagePreviousNextSelector_TemplateContainer.
+            Q<VisualElement>("ChevronLeft_TemplateContainer");
+        language_Lable = languagePreviousNextSelector_TemplateContainer.Q<Label>("Text_Label");
+        language_ChevronRight_TemplateContainer = languagePreviousNextSelector_TemplateContainer.
+            Q<VisualElement>("ChevronRight_TemplateContainer");
         FixPreviousNextSelectorDimentions(languagePreviousNextSelector_TemplateContainer);
 
-        fontSizePreviousNextSelector_TemplateContainer =
-            settings_ScrollView.Q<VisualElement>("FontSizePreviousNextSelector_TemplateContainer");
+        //FontSize
+        fontSizePreviousNextSelector_TemplateContainer = settings_ScrollView.
+            Q<VisualElement>("FontSizePreviousNextSelector_TemplateContainer");
+        fontSize_ChevronLeft_TemplateContainer = fontSizePreviousNextSelector_TemplateContainer.
+            Q<VisualElement>("ChevronLeft_TemplateContainer");
+        fontSize_Lable = fontSizePreviousNextSelector_TemplateContainer.Q<Label>("Text_Label");
+        fontSize_ChevronRight_TemplateContainer = fontSizePreviousNextSelector_TemplateContainer.
+            Q<VisualElement>("ChevronRight_TemplateContainer");
         FixPreviousNextSelectorDimentions(fontSizePreviousNextSelector_TemplateContainer);
+
+        AddFunctionality();
 
         OnLanguageChanged();
         OnFontSizeChanged();
@@ -58,6 +81,7 @@ public class SettingsPage : MonoBehaviour
     {
         previousNextSelector.style.width = Length.Percent(100);
         previousNextSelector.style.height = Screen.width / 10;
+        previousNextSelector.style.marginBottom = Length.Percent(1);
 
         VisualElement chevronLeft_TemplateContainer =
             previousNextSelector.Q<VisualElement>("ChevronLeft_TemplateContainer");
@@ -70,6 +94,83 @@ public class SettingsPage : MonoBehaviour
         chevronRight_TemplateContainer.style.width = Screen.width / 15;
         chevronRight_TemplateContainer.style.height = Screen.width / 15;
     }
+
+
+    private void AddFunctionality()
+    {
+        //Language
+        language_ChevronLeft_TemplateContainer.RegisterCallback<ClickEvent>(OnLanguage_ChevronLeftSelected);
+        language_ChevronRight_TemplateContainer.RegisterCallback<ClickEvent>(OnLanguage_ChevronRightSelected);
+
+        //Font Size
+        fontSize_ChevronLeft_TemplateContainer.RegisterCallback<ClickEvent>(OnFontSize_ChevronLeftSelected);
+        fontSize_ChevronRight_TemplateContainer.RegisterCallback<ClickEvent>(OnFontSize_ChevronRightSelected);
+    }
+
+    private void RemoveFunctionality()
+    {
+        //Language
+        language_ChevronLeft_TemplateContainer.UnregisterCallback<ClickEvent>(OnLanguage_ChevronLeftSelected);
+        language_ChevronRight_TemplateContainer.UnregisterCallback<ClickEvent>(OnLanguage_ChevronRightSelected);
+
+        //Font Size
+        fontSize_ChevronLeft_TemplateContainer.UnregisterCallback<ClickEvent>(OnFontSize_ChevronLeftSelected);
+        fontSize_ChevronRight_TemplateContainer.UnregisterCallback<ClickEvent>(OnFontSize_ChevronRightSelected);
+    }
+
+    #region Language
+    private void OnLanguage_ChevronLeftSelected(ClickEvent clickEvent)
+    {
+        SettingsData.currentLanguageIndex--;
+        if (SettingsData.currentLanguageIndex < 0)
+        {
+            SettingsData.currentLanguageIndex = LanguageTextsData.languages.Count - 1;
+        }
+        EventsManager.InvokeOnLanguageChanged();
+    }
+
+    private void OnLanguage_ChevronRightSelected(ClickEvent clickEvent)
+    {
+        SettingsData.currentLanguageIndex++;
+        if (SettingsData.currentLanguageIndex >= LanguageTextsData.languages.Count)
+        {
+            SettingsData.currentLanguageIndex = 0;
+        }
+        EventsManager.InvokeOnLanguageChanged();
+    }
+    #endregion
+
+
+    private void OnFontSize_ChevronLeftSelected(ClickEvent evt)
+    {
+        SettingsData.currentFontSizeIndex--;
+        if (SettingsData.currentFontSizeIndex < 0)
+        {
+            SettingsData.currentFontSizeIndex = LanguageTextsData.fontSize_Text.Count - 1;
+        }
+        fontSizePreviousNextSelector_TemplateContainer.Q<Label>().text =
+            LanguageTextsData.fontSize_Text[SettingsData.currentFontSizeIndex].
+            FontSizeLanguage[SettingsData.currentLanguageIndex];
+        EventsManager.InvokeOnFontSizeChanged();
+    }
+
+    private void OnFontSize_ChevronRightSelected(ClickEvent evt)
+    {
+        SettingsData.currentFontSizeIndex++;
+        if (SettingsData.currentFontSizeIndex >= LanguageTextsData.fontSize_Text.Count)
+        {
+            SettingsData.currentFontSizeIndex = 0;
+        }
+        fontSizePreviousNextSelector_TemplateContainer.Q<Label>().text =
+            LanguageTextsData.fontSize_Text[SettingsData.currentFontSizeIndex].
+            FontSizeLanguage[SettingsData.currentLanguageIndex];
+        EventsManager.InvokeOnFontSizeChanged();
+    }
+
+
+
+
+
 
 
     #region Events Manager
